20fbd0fad865e6ec5d23263caf2f1f3e948ef249
Jonathan Kayizzi <jonahkayizzi@gmail.com>
2022-09-19 21:51:16 +0300

    Update js to remove a deleted book

--- JS/books.js
+++ JS/books.js
@@ -3,17 +3,17 @@ let books = localStorage.getItem('books') === null ?
   {
   title : 'The Hobbit',
   author: 'Shaker',
-  id: '1',
+  id: '0',
   },
   {
   title : 'Harry Potter',
   author: 'Jonathan',
-  id: '2',
+  id: '1',
   },
   {
   title : 'Art of War',
   author: 'San Tzu',
-  id: '3',
+  id: '2',
   },
 ] 
 : 
@@ -27,20 +27,22 @@ for(let i = 0; i < books.length; i++){
   <div class="book">
   <p class="title">${books[i].title}</p>
   <p class="author">${books[i].author}</p>
-  <button class="remove">Remove</button>
+  <button class="remove" onclick="deleteBook(${books[i].id})">Remove</button>
   <hr>
 </div>
   `;
 }
 
 const addBookbtn = document.querySelector('.add-btn');
+const deleteBookbtn = document.querySelector('.remove');
 const titleInput = document.querySelector('.title-input');
 const authorInput = document.querySelector('.author-input');
+
 addBookbtn.addEventListener('click', (e) => {
   books.push({
     title: titleInput.value,
     author: authorInput.value,
-    id: (books.length + 1).toString(),
+    id: (books.length).toString(),
   });
 
   bookSection.innerHTML += 
@@ -48,7 +50,7 @@ addBookbtn.addEventListener('click', (e) => {
   <div class="book">
   <p class="title">${titleInput.value}</p>
   <p class="author">${authorInput.value}</p>
-  <button class="remove">Remove</button>
+  <button class="remove" onclick="deleteBook(${books.length})">Remove</button>
   <hr>
 </div>
   `;
@@ -56,3 +58,13 @@ addBookbtn.addEventListener('click', (e) => {
   localStorage.setItem('books', JSON.stringify(books));
   console.log(books);
 });
+
+
+function deleteBook(bookID) {
+  
+ const bookDiv = document.querySelectorAll('.book')
+
+  books = books.filter((book) => (book.id !== bookID.toString()));
+  bookDiv[bookID].remove();
+}
+
