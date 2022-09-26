3f1a36ffbb587dfba72279647385dbed45d4bb22
joker69216 <hamaraad883@gmail.com>
2022-09-26 20:08:08 +0300

    fix linter errors

--- JS/modules/date.js
+++ JS/modules/date.js
@@ -5,15 +5,15 @@ const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
 
 const getDateTime = () => {
   const now = DateTime.now();
-  const hour = now.c.hour > 12 ? now.c.hour - 12 : now.c.hour;
+  const hour = now.c.hour > 12 ? now.c.hour - 12 : now.c.hour; 
   const hourCounter = now.c.hour > 12 ? 'PM' : 'AM';
   const minute = now.c.minute < 10 ? `0${now.c.minute}` : now.c.minute;
   const second = now.c.second < 10 ? `0${now.c.second}` : now.c.second;
 
   const dayCounter = now.c.day === 1 ? 'st' : (now.c.day === 2 ? 'nd' : (now.c.day === 3 ? 'rd' : 'th'));
 
-  return month[now.c.month - 1] + ' ' + now.c.day + dayCounter + ' ' + now.c.year + ', '+
+  return month[now.c.month - 1] + ' ' + now.c.day + dayCounter + ' ' + `${now.c.year}, '`+
    hour + ':' + minute + ':' + second + ' ' + hourCounter;
 };
 
-module.exports = { getDateTime };
+export default { getDateTime };
