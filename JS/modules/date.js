2644393949d678790b6d8b5a165f2e77dd341b39
joker69216 <hamaraad883@gmail.com>
2022-09-26 20:25:44 +0300

    change code to es6

--- JS/modules/date.js
+++ JS/modules/date.js
@@ -3,7 +3,7 @@ import { DateTime } from './luxon.js';
 const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
   'August', 'September', 'October', 'November', 'December'];
 
-export const getDateTime = () => {
+const getDateTime = () => {
   const now = DateTime.now();
   const hour = now.c.hour > 12 ? now.c.hour - 12 : now.c.hour;
   const hourCounter = now.c.hour > 12 ? 'PM' : 'AM';
@@ -12,6 +12,8 @@ export const getDateTime = () => {
 
   const dayCounter = now.c.day === 1 ? 'st' : (now.c.day === 2 ? 'nd' : (now.c.day === 3 ? 'rd' : 'th'));
   // eslint-disable-line prefer-template
-  return month[now.c.month - 1] + ' ' + now.c.day + dayCounter 
+  return month[now.c.month - 1] + ' '
   + hour + ':' + minute + ':' + second + ' ' + hourCounter;
 };
+
+export default getDateTime;
